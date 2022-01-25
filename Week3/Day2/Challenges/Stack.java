531d93a94974dca4325b93a316f31fe36affba85
neel-searce01 <neel.sanghvi@searce.com>
2022-01-25 18:54:18 +0530

    Challenge1 Day2 Completed

--- Week3/Day2/Challenges/Stack.java
+++ Week3/Day2/Challenges/Stack.java
@@ -1,3 +1,10 @@
+//Week3
+//Challenge: Day2
+//Question1
+//Stack
+
+import java.util.Scanner;
+
 public class Stack {
     private int maxSize;
     private int[] stackArray;
@@ -5,34 +12,71 @@ public class Stack {
     private int top;
     private int maxtop;
 
-    public MyStack(int size){
+    public Stack(int size){
         maxSize = size;
         maxArray = new int[maxSize];
         stackArray = new int[maxSize];
-        maxtop = -1;
+        maxtop = 0;
         top = -1;
     }
 
     public void push(int ele){
         if(top > maxSize)
-        System.out.println("Stack is Full");
-       
+            System.out.println("Stack is Full");
+
         stackArray[++top] = ele;
 
         if(top==-1)
-        maxArray[++maxtop] = ele;
-        
-        if(data > maxArray[maxtop])
-        maxArray[++maxtop] = ele;
+            maxArray[++maxtop] = ele;
+
+        if(ele > maxArray[maxtop])
+            maxArray[++maxtop] = ele;
     }
 
     public int pop(){
-        if(this.isEmpty())
-        return "Stack is Empty";
+        if(top == -1)
+            System.out.println("Stack is Empty");
 
         if(maxArray[maxtop] == stackArray[top])
-        return maxArray[maxtop--];
-        
+            return maxArray[maxtop--];
+
         return stackArray[top--];
+    }
+
+    public void getMax()
+    {
+        if(top==-1)
+            System.out.println("Stack is Empty!");
+        System.out.println(maxArray[maxtop]);
+    }
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number of operations you want to undergo:");
+        int ch = s.nextInt();
+        s.nextLine();
+        Stack stack = new Stack(ch);
+        System.out.println("Enter " + ch + " operations:");
+
+        for(int i = 0; i < ch; i++){
+            String str = s.nextLine(); // "1 90", "2", "1 20"
+            String[] strArr = str.split(" ");
+            int[] numArr = new int[strArr.length];
+
+            for(int j=0; j <strArr.length; j++)
+                numArr[j] = Integer.parseInt(strArr[j]);
+
+            if(numArr[0] == 1)
+                stack.push(numArr[1]);
+
+            else if(numArr[0] == 2)
+                stack.pop();
+
+            else if(numArr[0] == 3)
+                stack.getMax();
+
+            else
+                System.out.println("Invalid Choice!");
+        }
     }
 }
